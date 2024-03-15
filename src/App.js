2554a639aeb8d498614afa783a139d3fc53894e5
dawnsoapp <madi.jackson29@gmail.com>
2024-03-15 14:26:19 -0700

    added some images to start with on a cute poke profile website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import dragapult1 from './images/dragapult1.png';
+import hitmonchan1 from './images/hitmonchan1.png';
+import smilotic1 from './images/smilotic1.png';
+import tinkaton1 from './images/tinkaton1.png';
+import tyranitar1 from './images/tyranitar1.png';
+import vikavolt1 from './images/vikavolt1.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Trainer Dawn's Team</h1>
+        <div className="Poke-container">
+        <img src={tinkaton1} className="Poke-logo" alt=""/>
+        <img src={tyranitar1} className="Poke-logo" alt=""/>
+        <img src={hitmonchan1} className="Poke-logo" alt=""/>
+        <img src={vikavolt1} className="Poke-logo" alt=""/>
+        <img src={smilotic1} className="Poke-logo" alt=""/>
+        <img src={dragapult1} className="Poke-logo" alt=""/>
+        </div>
         <p>
-          Madi is going to change this in the future"
-          Edit <code>src/App.js</code> and save to reload.
+          The stories behind the bonds between trainer and pokemon!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
